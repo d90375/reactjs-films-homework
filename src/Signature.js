eea702f69fb65d4898973bec05eede07c1bab7d8
d90375 <d90375@gmail.com>
2020-06-27 20:56:23 +0300

    feat: add eslint/prettier  config

--- src/Signature.js
+++ src/Signature.js
@@ -3,8 +3,13 @@ import PropTypes from "prop-types";
 import "./style.scss";
 
 const Signature = ({ name }) => {
-  return <div className='wrap'><span className='text'>{name}</span></div>;
+  return (
+    <div className="wrap">
+      <span className="text">{name}</span>
+    </div>
+  );
 };
+
 
 Signature.propTypes = {
   name: PropTypes.string.isRequired
