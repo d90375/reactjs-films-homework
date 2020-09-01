1c56fdbfc52a940359861fe05bafd30250d4bce1
d90375 <d90375@gmail.com>
2020-09-01 19:29:52 +0300

    fix: resolve conflict

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { hot } from "react-hot-loader/root";
 import Footer from "./Footer";
 import MovieList from "../containers/MovieList";
-import "./App.scss";
 import TopScreenContainer from "../containers/TopScreen";
 
 const App = () => {
